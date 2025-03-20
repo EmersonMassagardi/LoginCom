0603517cbef7751f3a521e4a7f741e2011408ac7
ArthurSilva0412 <134655344+ArthurSilva0412@users.noreply.github.com>
2025-03-19 22:38:05 -0300

    Javascript login

---
+++ js.js
@@ -0,0 +1,24 @@
+
+function validarLogin() {
+    let usuario = document.getElementById("usuario").value;
+    let senha = document.getElementById("senha").value;
+
+    document.getElementById("erroUsuario").textContent = "";
+    document.getElementById("erroSenha").textContent = "";
+
+    let valido = true;
+
+    if (usuario.trim() === "") {
+        document.getElementById("erroUsuario").textContent = "Preencha o usuário.";
+        valido = false;
+    }
+
+    if (senha.trim() === "") {
+        document.getElementById("erroSenha").textContent = "Preencha a senha.";
+        valido = false;
+    }
+
+    if (valido) {
+        alert("Login realizado com sucesso!");
+    }
+}
